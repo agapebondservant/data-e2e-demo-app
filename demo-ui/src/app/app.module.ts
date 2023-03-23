8af71d54c781d2007749b63ef35985b42ef8751d
Nikhil N <nn2@vmware.com>
2023-03-23 17:20:52 +0530

    Updating readme

--- demo-ui/src/app/app.module.ts
+++ demo-ui/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { DatePipe } from '@angular/common';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
