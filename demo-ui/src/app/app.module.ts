23e23e5c17815997e5de0dd1e815cd99d23aaa7c
Nikhil N <nn2@vmware.com>
2023-03-31 21:07:04 +0530

    Deployment changes

--- demo-ui/src/app/app.module.ts
+++ demo-ui/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { ClarityModule } from '@clr/angular';
+
 import * as CanvasJSAngularChart from 'src/assets/canvasjs.angular.component';
 var CanvasJSChart = CanvasJSAngularChart.CanvasJSChart;
 	
