e97638a6eaa90b6e73cb4a4353b12a3abd6ed2d7
Tola Awofolu <oawofolu@pivotal.io>
2023-09-20 07:58:54 -0400

    Added inference sql

--- demo-ml/resources/random_forest_madlib.sql
+++ demo-ml/resources/random_forest_madlib.sql
@@ -1,3 +1,7 @@
+---------------------------------------------------------------------------------
+-- TRAINING
+---------------------------------------------------------------------------------
+
 DROP FUNCTION run_random_forest_training();
 
 CREATE OR REPLACE FUNCTION public.run_random_forest_training()
@@ -9,7 +13,7 @@ RETURNS table (
     longitude real,
     is_fraud_flag real,
     training_run_timestamp bigint,
-    cls_weight_label smallint
+    cls_weight_label SMALLINT
 )
 as $BODY$
 BEGIN
@@ -28,8 +32,8 @@ BEGIN
 		amt real,
 		lat real,
 		long real,
-		is_fraud smallint,
-		cls_weight_label smallint);
+		is_fraud SMALLINT,
+		cls_weight_label SMALLINT);
 
 	-- Inference data
 	create table rf_credit_card_transactions_inference(
@@ -38,8 +42,8 @@ BEGIN
 		amt real,
 		lat real,
 		long real,
-		is_fraud smallint,
-		cls_weight_label smallint);
+		is_fraud SMALLINT,
+		cls_weight_label SMALLINT);
 
 
 	-- 2. ingest data into training table
@@ -104,7 +108,7 @@ BEGIN
 	g.long::real as longitude,
 	p.estimated_is_fraud::real as is_fraud_flag,
 	m.training_time::bigint as training_run_timestamp,
-	g.cls_weight_label::smallint as cls_weight
+	g.cls_weight_label::SMALLINT as cls_weight
 	FROM rf_credit_card_transactions_inference_results p,
 	rf_credit_card_transactions_inference g,
 	metadata m
@@ -112,4 +116,60 @@ BEGIN
 	ORDER BY g.id;
 END;
 $BODY$
+LANGUAGE plpgsql;
+
+---------------------------------------------------------------------------------
+-- PREDICTION
+---------------------------------------------------------------------------------
+DROP FUNCTION setup_madlib_tmp_source_table;
+DROP FUNCTION setup_madlib_tmp_prediction_table;
+DROP FUNCTION run_random_forest_prediction;
+
+CREATE OR REPLACE FUNCTION setup_madlib_tmp_source_table(table_prefix VARCHAR, id BIGINT, time_elapsed BIGINT, amt real, lat real, long real, cls_weight_label int, is_fraud SMALLINT)
+RETURNS VOID
+AS
+$BODY$
+BEGIN
+	EXECUTE format('create table "tmptbl_%s" as select * from (values (%L::bigint,%L::bigint,%L::real,%L::real,%L::real,%L::int,%L::SMALLINT))  t(id, time_elapsed, amt, lat, long, cls_weight_label, is_fraud)',
+			table_prefix,
+			id,
+			time_elapsed,
+			amt,
+			lat,
+			long,
+			cls_weight_label,
+			is_fraud);
+END;
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION setup_madlib_tmp_prediction_table(table_prefix VARCHAR) 
+RETURNS void
+AS
+$BODY$
+DECLARE
+	done VARCHAR;
+BEGIN
+	EXECUTE format('SELECT madlib.forest_predict(''rf_credit_card_transactions_model'',''tmptbl_%s'',''tmp_prediction_results_%s'',''response'')',table_prefix,table_prefix);
+END;
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION run_random_forest_prediction(time_elapsed BIGINT, amt REAL, lat REAL, long REAL, cls_weight_label INT DEFAULT 0, is_fraud SMALLINT DEFAULT NULL, id BIGINT DEFAULT 1)
+RETURNS SMALLINT AS
+$$
+DECLARE
+	table_prefix VARCHAR;
+	done VARCHAR;
+	result SMALLINT;
+BEGIN
+	SELECT TO_CHAR(CURRENT_TIMESTAMP, 'YYYYMMDDHH12MMSS') INTO table_prefix;
+	SELECT setup_madlib_tmp_source_table(table_prefix, id, time_elapsed, amt, lat, long, cls_weight_label, is_fraud) INTO done;
+	SELECT setup_madlib_tmp_prediction_table(table_prefix) INTO done;
+	EXECUTE format('SELECT estimated_is_fraud FROM "tmp_prediction_results_%s" p, "tmptbl_%s" g WHERE p.id = g.id ORDER BY g.id ',
+					table_prefix,
+				    table_prefix) INTO result;
+	RETURN result;
+
+end $$
 LANGUAGE plpgsql;
