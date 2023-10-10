4ad4154b4f75a808e43393be61522a4f8fa0f10a
Tola Awofolu <oawofolu@pivotal.io>
2023-10-10 01:52:39 -0400

    Renamed files

--- demo-ml/resources/deploy_inference_db.sh
+++ demo-ml/resources/deploy_inference_db.sh
@@ -1,1 +1,2 @@
-sed -i "s/XYZCHANGESETID/$(date +%s)/g; s/XYZDBSCHEMA/${DB_SCHEMA}/g;" ${SHARED_PATH}/${DB_SCRIPT}
+chmod +w ${SHARED_PATH}/${DB_SCRIPT}
+sed -i "" "s/XYZCHANGESETID/$(date +%s)/g; s/XYZDBSCHEMA/${DB_SCHEMA}/g;" ${SHARED_PATH}/${DB_SCRIPT}
