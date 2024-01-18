62c4f698f11161f8285a3641bf489a430171b58b
Tola Awofolu <oawofolu@pivotal.io>
2024-01-17 19:10:55 -0500

    Updated Minio scripts

--- deploy/templates/scripts/deploy-argo-minio-server.sh
+++ deploy/templates/scripts/deploy-argo-minio-server.sh
@@ -8,15 +8,13 @@ helm repo add minio-legacy https://helm.min.io/
 
 kubectl create ns ${MINIO_BUCKET_NAMESPACE}
 
-helm install --set resources.requests.memory=1.5Gi,tls.enabled=false --namespace ${MINIO_BUCKET_NAMESPACE} minio minio-legacy/minio --set service.type=LoadBalancer --set service.port=9000
+helm install --set resources.requests.memory=1.5Gi,auth.rootUser=minio,ingress.enabled=true,ingress.hostname="minio-argo.${DATA_E2E_FQDN_DOMAIN}" --namespace ${MINIO_BUCKET_NAMESPACE} minio oci://registry-1.docker.io/bitnamicharts/minio --wait
 
 kubectl apply -f deploy/templates/demo-minio/minio-http-proxy.yaml --namespace ${MINIO_BUCKET_NAMESPACE}
 
-export MINIO_ARGO_ACCESS_KEY_ID=$(kubectl get secret minio -o jsonpath="{.data.accesskey}" -n ${MINIO_BUCKET_NAMESPACE}| base64 --decode)
+export MINIO_ARGO_ACCESS_KEY_ID=$(kubectl get secret minio -o jsonpath="{.data.root-user}" -n ${MINIO_BUCKET_NAMESPACE}| base64 --decode)
 
-export MINIO_ARGO_SECRET_ACCESS_KEY=$(kubectl get secret minio -o jsonpath="{.data.secretkey}" -n ${MINIO_BUCKET_NAMESPACE}| base64 --decode)
-
-kubectl wait --for=condition=Ready pod -l app=minio -n ${MINIO_BUCKET_NAMESPACE} --timeout=120s
+export MINIO_ARGO_SECRET_ACCESS_KEY=$(kubectl get secret minio -o jsonpath="{.data.root-password}" -n ${MINIO_BUCKET_NAMESPACE}| base64 --decode)
 
 sleep 5
 
@@ -24,6 +22,6 @@ kubectl apply -f deploy/templates/demo-minio/minio-secret-exporter.yaml -n ${MIN
 
 kubectl apply -f deploy/templates/demo-minio/minio-secret-importer.yaml -n argo
 
-mc config host add --insecure data-e2e-minio-argo http://${ARGO_S3_ENDPOINT_FQDN} ${MINIO_ARGO_ACCESS_KEY_ID} ${MINIO_ARGO_SECRET_ACCESS_KEY}
+mc config host add --insecure data-e2e-minio-argo http://minio-argo.${DATA_E2E_FQDN_DOMAIN} ${MINIO_ARGO_ACCESS_KEY_ID} ${MINIO_ARGO_SECRET_ACCESS_KEY}
 
 mc mb --insecure -p data-e2e-minio-argo/demo && mc policy --insecure set public data-e2e-minio-argo/demo
